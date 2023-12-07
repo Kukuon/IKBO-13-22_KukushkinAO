2c8c6ce807ada65812664b772eb4957ce9d7378a
Kukuon <kukuon04@gmail.com>
2023-12-07 18:39:19 +0300

    3

--- java-script/slider-script.js
+++ java-script/slider-script.js
@@ -1,18 +1,16 @@
-let currentIndex = 0;
+let currentSlide = 0;
 
-function changeCar(direction) {
-  const slider = document.querySelector('.slider');
-  const cars = document.querySelectorAll('.slide');
-  const sliderWidth = document.querySelector('.slider-container').offsetWidth;
+function showSlide(index) {
+    const slider = document.querySelector('.slider');
+    const slides = document.querySelectorAll('.slide');
+    const slideWidth = slides[0].offsetWidth;
 
-  currentIndex += direction;
+    if (index < 0) {
+        index = slides.length - 1;
+    } else if (index >= slides.length) {
+        index = 0;
+    }
 
-  if (currentIndex < 0) {
-    currentIndex = cars.length - 1;
-  } else if (currentIndex >= cars.length) {
-    currentIndex = 0;
-  }
-
-  const displacement = -currentIndex * sliderWidth;
-  slider.style.transform = `translateX(${displacement}px)`;
+    currentSlide = index;
+    slider.style.transform = `translateX(${-index * slideWidth}px)`;
 }
